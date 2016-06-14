ac545fa61f6b227e32c27376364c99cb176bcd9a
TakesxiSximada <takesxi.sximada@gmail.com>
2016-06-14 23:31:52 +0900

    implemented syaml

---
+++ src/syaml/settings.py
@@ -0,0 +1,59 @@
+import os
+import logging
+
+import yaml
+from mako.template import Template
+from zope.interface import implementer
+from zope.component import IFactory
+
+from .volumes import Volume
+from .interfaces import ISetting
+
+_logger = logging.getLogger(__name__)
+
+
+@implementer(ISetting)
+class VolumeSetting(dict):
+    def __init__(self, *args, **kwds):
+        super(VolumeSetting, self).__init__(*args, **kwds)
+        self.project_name = None  # docker-compose project
+        self.name = None
+        self.dry_run = False
+
+    def get_volume_name(self, name):
+        if name not in self:
+            raise KeyError(name)
+        return '{}_{}'.format(self.project_name, name)
+
+    def set_name(self, name):
+        self.name = name
+
+    def set_dry_run(self):
+        self.dry_run = True
+
+
+@implementer(IFactory)
+class VolumeSettingParser(object):
+    def __init__(self, env, path, project_name=''):
+        self.env = env
+        self.path = path
+        self.project_name = project_name
+
+    def __call__(self, env=None, path=None, project_name=None):
+        env = self.env if env is None else env
+        path = self.path if path is None else path
+        project_name = self.project_name if project_name is None else project_name
+
+        kwds = dict(os.environ)
+        kwds['here'] = os.path.abspath(os.path.dirname(path))
+
+        tmpl = Template(filename=path)
+        data = yaml.load(tmpl.render(**kwds))
+
+        setting = VolumeSetting()
+        setting.set_name(env)
+        setting.project_name = project_name
+        for volume_name, volume_data in data['volumes'].items():
+            volume = Volume(**volume_data)
+            setting[volume_name] = volume
+        return setting
---
+++ src/syaml/syaml.py
@@ -0,0 +1,68 @@
+import io
+import os
+import functools
+
+import six
+import yaml
+from mako.template import Template
+from zope.interface import implementer
+
+from .interfaces import (
+    IParser,
+    IPostProcess,
+    IPreProcess,
+    IReader,
+    )
+
+
+@implementer(IPreProcess)
+class SyamlPreProcess(object):
+    def __call__(self, fileobj):
+        if isinstance(fileobj, six.text_type):
+            filepath = fileobj
+            tmpl = Template(filename=filepath)
+        else:  # may be file like object
+            filepath = fileobj.name
+            tmpl = Template(fileobj.read())
+
+        abs_file_path = os.path.abspath(filepath)
+        kwds = dict(os.environ)
+        kwds['here'] = os.path.dirname(abs_file_path)
+        kwds['name'] = os.path.basename(abs_file_path)
+        kwds['path'] = abs_file_path
+        fp = io.BytesIO(tmpl.render(**kwds))
+        fp.seek(0)
+        return fp
+
+
+@implementer(IParser)
+class SyamlParser(object):
+    def __call__(self, fileobj):
+        return yaml.load(fileobj.read())
+
+
+@implementer(IPostProcess)
+class SyamlPostProcess(object):
+    def __call__(self, obj):
+        return obj
+
+
+@implementer(IReader)
+class SyamlReader(object):
+    def __init__(self, pre, parse, post):
+        self.pre = pre
+        self.parse = parse
+        self.post = post
+
+    def __call__(self, fileobj):
+        return functools.reduce(
+            lambda obj, fun: fun(obj),
+            (self.pre, self.parse, self.post),
+            fileobj,
+            )
+
+read = SyamlReader(
+    pre=SyamlPreProcess(),
+    parser=SyamlParser(),
+    post=SyamlPostProcess(),
+    )
